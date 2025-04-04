cb2b3adf9ec87d54c44b556f5b6b9d29f6b0e665
Luiz Felipe Santos Lopes <luizphelip1998@gmail.com>
2025-04-04 16:44:16 -0300

    Criando a classe de exceção ParametrosInvalidosException.java

---
+++ src/ParametrosInvalidosException.java
@@ -0,0 +1,8 @@
+public class ParametrosInvalidosException extends RuntimeException{
+    public ParametrosInvalidosException () {
+        super();
+    }
+    public ParametrosInvalidosException (String mensagem) {
+        super(mensagem);
+    }
+}
